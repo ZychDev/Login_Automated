e1ee3dd3fa63f8c006b09cbe89dcb007d88475a9
ZychDev <szymonzychdev@gmail.com>
2021-02-17 13:24:31 +0100

    Sign in page

--- login.py
+++ login.py
@@ -6,3 +6,9 @@ web_driver = webdriver.Chrome('/usr/bin/chromedriver')
 
 #open github
 web_driver.get("https://github.com/")
+
+#Sign in
+for element in web_driver.find_elements_by_tag_name("a"):
+    if "/login" in element.get_attribute('href'):
+        element.click()
+
