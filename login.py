98a9d47e9f051bd19d7030858dddce3cec82ef63
ZychDev <szymonzychdev@gmail.com>
2021-02-17 14:49:49 +0100

    login stackoverflow

--- login.py
+++ login.py
@@ -7,21 +7,19 @@ passw = '.....'
 web_driver = webdriver.Chrome('/usr/bin/chromedriver')
 
 #open github
-web_driver.get("https://github.com/")
-
+web_driver.get("https://stackoverflow.com")
 #Sign in
 for element in web_driver.find_elements_by_tag_name("a"):
     if "/login" in element.get_attribute('href'):
         element.click()
+        
 
-time.sleep(1)
+        sleep(1)
 
-user_field = web_driver.find_element_by_id('login_field')
-user_field.send_keys(mail)
-time.sleep(1)
+        username = web_driver.find_element_by_id('email')
+        username.send_keys("name")
 
-password_field = web_driver.find_element_by_id('password')
-password_field.send_keys(passw)
-time.sleep(1)
+        password_field = web_driver.find_element_by_id('password')
+        password_field.send_keys("password")
 
-driver.find_element_by_xpath('//*[@class="btn btn-primary btn-block"]').click()
+        web_driver.find_element_by_xpath('//*[@id="submit-button"]').click()    
